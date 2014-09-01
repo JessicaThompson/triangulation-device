8a218c10638b67a8696a841678b8e89a6aab10b1
Chris Vandevelde <chris.vandevelde@uwaterloo.ca>
2014-09-01 16:45:40 -0400

    Encapsulated the PD stuff in its own class for embedding.

---
+++ src/de/vndvl/chrs/triangulationdevice/pd/PDDriver.java
@@ -0,0 +1,184 @@
+package de.vndvl.chrs.triangulationdevice.pd;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.service.PdService;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.location.Location;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import de.vndvl.chrs.triangulationdevice.R;
+
+public class PDDriver {
+
+    private final Context context;
+    private PdUiDispatcher dispatcher;
+    private PdService pdService;
+
+    private Location myLocation;
+    private Location theirLocation;
+
+    private final ServiceConnection pdConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            PDDriver.this.pdService = ((PdService.PdBinder) service).getService();
+            try {
+                initPd();
+                loadPatch();
+            } catch (IOException e) {
+                Log.e(getClass().toString(), e.toString());
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // Apparently this method will never be called?
+        }
+    };
+
+    public PDDriver(Context context) {
+        this.context = context;
+    }
+
+    public void initServices() {
+        Intent pdIntent = new Intent(this.context, PdService.class);
+        this.context.bindService(pdIntent, this.pdConnection, Context.BIND_AUTO_CREATE);
+
+        TelephonyManager telephonyManager = (TelephonyManager) this.context.getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                if (PDDriver.this.pdService == null)
+                    return;
+                if (state == TelephonyManager.CALL_STATE_IDLE) {
+                    startPdAudio();
+                    // TODO: Handle possible edge case
+                    // Person has app open, sound off, receives call
+                    // (undesired sound starts when call ends?)
+                } else {
+                    PDDriver.this.pdService.stopAudio();
+                }
+            }
+        }, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    public void start() {
+        startPdAudio();
+        myLocationChanged(this.myLocation);
+        pdChangeXfade(0f);
+        PdBase.sendBang("trigger");
+    }
+
+    public void stop() {
+        if (this.pdService != null) {
+            this.pdService.stopAudio();
+        }
+    }
+
+    public void close() {
+        this.context.unbindService(this.pdConnection);
+    }
+
+    private void initPd() throws IOException {
+        int sampleRate = AudioParameters.suggestSampleRate();
+        int inChannels = 0;
+        int outChannels = 2;
+        float bufferSize = 10;
+        this.pdService.initAudio(sampleRate, inChannels, outChannels, bufferSize);
+        this.dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(this.dispatcher);
+    }
+
+    private void startPdAudio() {
+        if (!this.pdService.isRunning()) {
+            Intent intent = new Intent(this.context, this.context.getClass());
+            this.pdService.startAudio(intent, R.drawable.ab_logo, "Triangulation Device", "Return to Triangulation Device");
+            // Starts audio and creates a notification pointing to this activity
+            // To start audio with no notification, give startAudio() 0 args
+        }
+    }
+
+    private void loadPatch() throws IOException {
+        File dir = this.context.getFilesDir();
+        IoUtils.extractZipResource(this.context.getResources().openRawResource(R.raw.triangulationdevice_comp), dir, true);
+        File patchFile = new File(dir, "triangulationdevice_comp.pd");
+        PdBase.openPatch(patchFile.getAbsolutePath());
+    }
+
+    public void theirLocationChanged(Location location) {
+        this.theirLocation = location;
+        pdChangeProximity(location, this.theirLocation);
+    }
+
+    public void myLocationChanged(Location location) {
+        this.myLocation = location;
+
+        // I know you can convert to HMS format, but using that requires String
+        // conversion & manipulation...
+        // TODO: Use formatted strings to extract HMS rather than tons of math
+        // TODO: Split extractHMS to lat OR long rather than lat AND long?
+        HashMap<String, Float> gpsNamesToVals = extractHMS(location);
+        for (HashMap.Entry<String, Float> entry : gpsNamesToVals.entrySet()) {
+            PdBase.sendFloat(entry.getKey(), entry.getValue());
+        }
+
+        if (this.theirLocation != null) {
+            pdChangeProximity(location, this.theirLocation);
+        }
+    }
+
+    public void pdChangeProximity(Location myLocation, Location theirLocation) {
+        HashMap<String, Float> myHMS = extractHMS(myLocation);
+        HashMap<String, Float> theirHMS = extractHMS(theirLocation);
+
+        float proxlat = Math.abs(myHMS.get("lats") - theirHMS.get("lats"));
+        float proxlong = Math.abs(myHMS.get("longs") - theirHMS.get("longs"));
+
+        PdBase.sendFloat("proxlat", proxlat);
+        PdBase.sendFloat("proxlong", proxlong);
+    }
+
+    public HashMap<String, Float> extractHMS(Location location) {
+        // Returns a HashMap of H, M, S doubles (values)
+        // mapped to their Pd variable names (keys)
+        HashMap<String, Float> result = new HashMap<String, Float>();
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        double lath = Math.floor(latitude);
+        double longh = Math.floor(longitude);
+        double latm = Math.floor(60d * (latitude - lath));
+        double longm = Math.floor(60d * (longitude - longh));
+        double lats = Math.floor(3600d * (latitude - lath - latm / 60d));
+        double longs = Math.floor(3600d * (longitude - longh - longm / 60d));
+
+        result.put("lath", (float) lath);
+        result.put("longh", (float) longh);
+        result.put("latm", (float) latm);
+        result.put("longm", (float) longm);
+        result.put("lats", (float) lats);
+        result.put("longs", (float) longs);
+        return result;
+    }
+
+    public void pdChangeXfade(float level) {
+        // Change the xFade between the two users
+        // 0 = 100% user1 (me/my)
+        // 1 = 100% user2 (them/their)
+        if (0 <= level && level <= 1) {
+            // THE LEVEL HAS TO BE IN THE RANGE OF (0,1)
+            PdBase.sendFloat("xfade", level);
+        }
+    }
+}
--- src/de/vndvl/chrs/triangulationdevice/ui/MapActivity.java
+++ src/de/vndvl/chrs/triangulationdevice/ui/MapActivity.java
@@ -1,34 +1,17 @@
 package de.vndvl.chrs.triangulationdevice.ui;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-
-import org.puredata.android.io.AudioParameters;
-import org.puredata.android.service.PdService;
-import org.puredata.android.utils.PdUiDispatcher;
-import org.puredata.core.PdBase;
-import org.puredata.core.utils.IoUtils;
-
 import android.app.AlertDialog;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.os.Parcelable.Creator;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import de.vndvl.chrs.triangulationdevice.R;
+import de.vndvl.chrs.triangulationdevice.pd.PDDriver;
 import de.vndvl.chrs.triangulationdevice.storage.PathStorage;
 import de.vndvl.chrs.triangulationdevice.ui.partial.BluetoothIPCActivity;
 import de.vndvl.chrs.triangulationdevice.ui.views.DraggableWeightView;
@@ -41,6 +24,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
 
     private boolean recording = false;
     private final PathStorage path = new PathStorage(this);
+    private final PDDriver pd = new PDDriver(this);
 
     private WaveformView myWaveform;
     private WaveformView theirWaveform;
@@ -52,152 +36,14 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
 
     private DraggableWeightView waveforms;
 
-    private Location theirLocation;
-
-    /* Pd code begins here */
-
-    private PdUiDispatcher dispatcher;
-    private PdService pdService = null;
-
-    private final ServiceConnection pdConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            MapActivity.this.pdService = ((PdService.PdBinder) service).getService();
-            try {
-                // Initialize PD, load patch
-                initPd();
-                loadPatch();
-            } catch (IOException e) {
-                Log.e(getClass().toString(), e.toString());
-                finish();
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            // Apparently this method will never be called?
-        }
-    };
-
-    private void initPd() throws IOException {
-        int sampleRate = AudioParameters.suggestSampleRate();
-        this.pdService.initAudio(sampleRate, 0, 2, 10.0f);
-        // sampleRate, inChannels, outChannels, bufferSize [ms]
-        this.dispatcher = new PdUiDispatcher();
-        PdBase.setReceiver(this.dispatcher);
-        /* TODO: Listeners go here (if necessary) */
-    }
-
-    private void startPdAudio() {
-        if (!this.pdService.isRunning()) {
-            Intent intent = new Intent(MapActivity.this, MapActivity.class);
-            this.pdService.startAudio(intent, R.drawable.ab_logo, "Triangulation Device", "Return to Triangulation Device");
-            // Starts audio and creates a notification pointing to this activity
-            // To start audio with no notification, give startAudio() 0 args
-        }
-    }
-
-    private void initSystemServices() {
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        telephonyManager.listen(new PhoneStateListener() {
-            @Override
-            public void onCallStateChanged(int state, String incomingNumber) {
-                if (MapActivity.this.pdService == null)
-                    return;
-                if (state == TelephonyManager.CALL_STATE_IDLE) {
-                    startPdAudio();
-                    // TODO: Handle possible edge case
-                    // Person has app open, sound off, receives call
-                    // (undesired sound starts when call ends?)
-                } else {
-                    MapActivity.this.pdService.stopAudio();
-                }
-            }
-        }, PhoneStateListener.LISTEN_CALL_STATE);
-    }
-
-    private void loadPatch() throws IOException {
-        File dir = getFilesDir();
-        IoUtils.extractZipResource(getResources().openRawResource(R.raw.triangulationdevice_comp), dir, true);
-        File patchFile = new File(dir, "triangulationdevice_comp.pd");
-        PdBase.openPatch(patchFile.getAbsolutePath());
-    }
-
-    public void pdChangeMyLocation(Location location) {
-        // I know you can convert to HMS format, but using that requires String
-        // conversion & manipulation...
-        // TODO: Use formatted strings to extract HMS rather than tons of math
-        // TODO: Split extractHMS to lat OR long rather than lat AND long?
-        Log.i(getClass().toString(), "pdChangeMyLocation " + location.toString());
-        HashMap<String, Float> gpsNamesToVals = extractHMS(location);
-        for (HashMap.Entry<String, Float> entry : gpsNamesToVals.entrySet()) {
-            PdBase.sendFloat(entry.getKey(), entry.getValue());
-        }
-
-        if (this.theirLocation != null) {
-            pdChangeProximity(location, this.theirLocation);
-        }
-    }
-
-    public void pdChangeProximity(Location myLocation, Location theirLocation) {
-        Log.i(getClass().toString(), "pdChangeProximity " + myLocation.toString());
-        Log.i(getClass().toString(), "pdChangeProximity " + theirLocation.toString());
-        HashMap<String, Float> myHMS = extractHMS(myLocation);
-        HashMap<String, Float> theirHMS = extractHMS(theirLocation);
-
-        float proxlat = Math.abs(myHMS.get("lats") - theirHMS.get("lats"));
-        float proxlong = Math.abs(myHMS.get("longs") - theirHMS.get("longs"));
-
-        PdBase.sendFloat("proxlat", proxlat);
-        PdBase.sendFloat("proxlong", proxlong);
-    }
-
-    public HashMap<String, Float> extractHMS(Location location) {
-        // Returns a HashMap of H, M, S doubles (values)
-        // mapped to their Pd variable names (keys)
-        HashMap<String, Float> result = new HashMap<String, Float>();
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        double lath = Math.floor(latitude);
-        double longh = Math.floor(longitude);
-        double latm = Math.floor(60d * (latitude - lath));
-        double longm = Math.floor(60d * (longitude - longh));
-        double lats = Math.floor(3600d * (latitude - lath - latm / 60d));
-        double longs = Math.floor(3600d * (longitude - longh - longm / 60d));
-
-        result.put("lath", (float) lath);
-        result.put("longh", (float) longh);
-        result.put("latm", (float) latm);
-        result.put("longm", (float) longm);
-        result.put("lats", (float) lats);
-        result.put("longs", (float) longs);
-        return result;
-    }
-
-    public void pdChangeXfade(float level) {
-        // Change the xFade between the two users
-        // 0 = 100% user1 (me/my)
-        // 1 = 100% user2 (them/their)
-        if (0 <= level && level <= 1) {
-            // THE LEVEL HAS TO BE IN THE RANGE OF (0,1)
-            PdBase.sendFloat("xfade", level);
-        }
-    }
-
-    /* Pd code ends here (for the most part) */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Bind & init PdService
-        bindService(new Intent(this, PdService.class), this.pdConnection, BIND_AUTO_CREATE);
-        initSystemServices();
-
         Typefaces.loadTypefaces(this);
-
         setContentView(R.layout.map_activity);
         this.resources = getResources();
+
+        this.pd.initServices();
 
         this.myWaveform = (WaveformView) findViewById(R.id.my_waveform);
         this.theirWaveform = (WaveformView) findViewById(R.id.their_waveform);
@@ -207,8 +53,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
         this.waveforms.setListener(new DraggableWeightView.Listener() {
             @Override
             public void onChanged(double topBottomRatio) {
-                double xfade = 1d - topBottomRatio;
-                pdChangeXfade((float) xfade);
+                MapActivity.this.pd.pdChangeXfade(1 - (float) topBottomRatio);
             }
         });
 
@@ -229,7 +74,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unbindService(this.pdConnection);
+        this.pd.close();
     }
 
     @Override
@@ -241,7 +86,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
             this.path.addMine(location);
         }
 
-        pdChangeMyLocation(location);
+        this.pd.myLocationChanged(location);
     }
 
     @Override
@@ -257,7 +102,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
             this.path.addTheirs(location);
         }
 
-        pdChangeProximity(getLocation(), this.theirLocation);
+        this.pd.theirLocationChanged(location);
     }
 
     @Override
@@ -269,6 +114,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
     @Override
     protected void onPause() {
         this.path.close();
+        this.pd.stop();
         super.onPause();
     }
 
@@ -317,12 +163,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
 
     public void start(View buttonView) {
         this.recording = true;
-
-        // start Pd patch
-        startPdAudio();
-        pdChangeMyLocation(getLocation());
-        pdChangeXfade(0f);
-        PdBase.sendBang("trigger");
+        this.pd.start();
 
         this.startStopButton.setText(this.resources.getString(R.string.stop));
         this.startStopButton.setBackgroundResource(R.drawable.stop_button);
@@ -336,9 +177,7 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
 
     public void stop(View buttonView) {
         this.recording = false;
-
-        // stop Pd patch
-        MapActivity.this.pdService.stopAudio();
+        this.pd.stop();
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Save Session");
@@ -359,7 +198,8 @@ public class MapActivity extends BluetoothIPCActivity<Location> {
         alert.setNegativeButton("Discard", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int whichButton) {
-                MapActivity.this.path.finish(null);
+                // Do nothing, don't save it.
+                // TODO: But still send it to the server?
             }
         });
         alert.show();
